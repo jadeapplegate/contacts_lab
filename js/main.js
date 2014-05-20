eb6e97028a26a13a23ed9a754eecb2ea09134065
Jade <jade.applegate@gmail.com>
2014-05-20 08:59:35 -0700

    deletes and edits

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	     var phone = $('#phone').val();
 	     var email = $('#email').val();
 	     var photo = $('#photo').val();
-	     contact = { cid: contactsLength(), name: name, phone: phone, email: email, photo: photo }
+	     var contact = { cid: contactsLength(), name: name, phone: phone, email: email, photo: photo }
        addContact(contact);
      };
    }); 
@@ -20,20 +20,37 @@ $(document).ready(function(){
 
 	var addContact = function(contact){
 		var photo ="<img src='"+contact.photo+"'>";
-		var editButton = "<span class = 'edit'>Edit</span>";
+		var editButton = "<span class='edit-contact'>Edit</span>";
 		var deleteButton = "<span class='delete-contact'>Delete</span>";
-		var newContact = "<li id='"+contact.cid+"'>" + photo + contact.name + " " + contact.phone + " " + contact.email + " " + editButton + " " + deleteButton + "</li>";
+		var newContact = "<li id='"+contact.cid+"' data-name='"+contact.name+"' data-phone='"+contact.phone+"' data-email='"+contact.email+"' data-photo='"+contact.photo+"'>" + photo + contact.name + " " + contact.phone + " " + contact.email + " " + editButton + " " + deleteButton + "</li>";
 		$(newContact).hide().appendTo('ul').fadeIn(500);
   };
 
 	 $('ul').on("click", ".delete-contact", function(){
 	 		$(this).parent().fadeOut(500, function(){
+	 			$(this).remove();
+	 		});
+	 });
+
+	 $('ul').on("click", ".edit-contact", function(){
+	 	  var name = $(this).parent().attr("data-name");
+	 	  $('#name').val(name);
+	 		var phone = $(this).parent().attr("data-phone");
+	 	  $('#phone').val(phone);
+	 	  var email = $(this).parent().attr("data-email");
+	 	  $('#email').val(email);
+	 	  var photo = $(this).parent().attr("data-photo");
+	 	  $('#photo').val(photo);
+
+	 		$(this).parent().fadeOut(500, function(){
+	 			$(this).remove();
+	 			this.reset;
 	 		});
 	 });
 
 	 var contactsLength = function(){
-	 	 $('li').length;
-	 }
+	 	 return $('li').length;
+	 };
 
 
 });
